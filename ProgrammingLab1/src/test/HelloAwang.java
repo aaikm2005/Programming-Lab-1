79a0cf3771200ca94218ccd2e6e6e40750316b79
USER <USER@LAPTOP-C7UEFL6T>
2024-10-16 09:32:52 +0800

    First Commit

---
+++ ProgrammingLab1/src/test/HelloAwang.java
@@ -0,0 +1,18 @@
+package test;
+
+public class HelloAwang {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double cents = 5.5;
+		
+		String awang = "\nAwang Aliff Ikmal\n";
+		
+			for(int counter = 0; counter <= 10; counter++) {
+				
+				System.out.println("Hello World!" + awang + counter + cents);
+			}
+	}
+
+}
