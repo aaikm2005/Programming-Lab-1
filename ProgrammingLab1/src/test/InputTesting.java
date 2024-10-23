ec9715afde0a403a02998408fac9edd04aff21e5
USER <USER@LAPTOP-C7UEFL6T>
2024-10-23 08:03:34 +0800

    THIRD COMMIT

---
+++ ProgrammingLab1/src/test/InputTesting.java
@@ -0,0 +1,16 @@
+package test;
+import java.util.Scanner;
+
+public class InputTesting {
+	public static void main(Strings[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter a number for radius of a circle: ");
+		double radius = scanner.nextDouble();
+		
+		
+		
+		
+		
+	}
+}
